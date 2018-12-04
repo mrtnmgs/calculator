8e849d52d56df4d5447aa65bc148637dbff710f3
Martin Maugeais <mrtnmgs@gmail.com>
2018-12-04 18:09:23 -0500

    add redux, model state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = { currentVal: 0, memoryVal: null };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_CURRENTVAL":
+      return state;
+    case "SET_MEMORYVAL":
+      return state;
+    case "CALCULATE":
+      return state;
+    case "CLEAR":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
