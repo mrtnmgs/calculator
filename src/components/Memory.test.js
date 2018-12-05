b3559d80907ecf12327a9274d76205a62bdeee6a
Martin Maugeais <mrtnmgs@gmail.com>
2018-12-04 20:04:47 -0500

    add smoke tests for components

--- src/components/Memory.test.js
+++ src/components/Memory.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import App from "./App";
+import Memory from "./Memory";
 
 const reducer = (state = { currentVal: "29/3", memoryVal: "95.33" }, action) =>
   state;
@@ -13,7 +13,7 @@ it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <Memory />
     </Provider>,
     div
   );
